ee71871a97499a7749e679c7fa91fef68df56410
mishudark <mishu.drk@gmail.com>
2020-06-22 16:02:33 +0200

    fix(asyn commandbus): make private methods

--- commandbus/async/async.go
+++ commandbus/async/async.go
@@ -5,9 +5,9 @@ import (
 	"github.com/mishudark/triper"
 )
 
-// Worker contains the basic info to manage commands.
-type Worker struct {
-	WorkerPool     chan chan triper.Command
+// worker contains the basic info to manage commands.
+type worker struct {
+	workerPool     chan chan triper.Command
 	JobChannel     chan triper.Command
 	CommandHandler triper.CommandHandlerRegister
 }
@@ -15,15 +15,15 @@ type Worker struct {
 // Bus stores the command handler.
 type Bus struct {
 	CommandHandler triper.CommandHandlerRegister
-	maxWorkers     int
+	maxworkers     int
 	workerPool     chan chan triper.Command
 }
 
-// Start initialize a worker ready to receive jobs.
-func (w *Worker) Start() {
+// start initialize a worker ready to receive jobs.
+func (w *worker) start() {
 	go func() {
 		for {
-			w.WorkerPool <- w.JobChannel
+			w.workerPool <- w.JobChannel
 
 			job := <-w.JobChannel
 			handler, err := w.CommandHandler.GetHandler(job)
@@ -42,15 +42,15 @@ func (w *Worker) Start() {
 	}()
 }
 
-// NewWorker initialize the values of worker and start it.
-func NewWorker(commandHandler triper.CommandHandlerRegister, workerPool chan chan triper.Command) {
-	w := Worker{
-		WorkerPool:     workerPool,
+// newWorker initialize the values of worker and start it.
+func newWorker(commandHandler triper.CommandHandlerRegister, workerPool chan chan triper.Command) {
+	w := worker{
+		workerPool:     workerPool,
 		CommandHandler: commandHandler,
 		JobChannel:     make(chan triper.Command),
 	}
 
-	w.Start()
+	w.start()
 }
 
 // HandleCommand ad a job to the queue.
@@ -66,21 +66,21 @@ func (b *Bus) HandleCommand(command triper.Command) (id string) {
 }
 
 // NewBus return a bus with command handler register.
-func NewBus(register triper.CommandHandlerRegister, maxWorkers int) *Bus {
+func NewBus(register triper.CommandHandlerRegister, maxworkers int) *Bus {
 	b := &Bus{
 		CommandHandler: register,
-		maxWorkers:     maxWorkers,
+		maxworkers:     maxworkers,
 		workerPool:     make(chan chan triper.Command),
 	}
 
 	// start the bus
-	b.Start()
+	b.start()
 	return b
 }
 
-// Start the bus
-func (b *Bus) Start() {
-	for i := 0; i < b.maxWorkers; i++ {
-		NewWorker(b.CommandHandler, b.workerPool)
+// start the bus
+func (b *Bus) start() {
+	for i := 0; i < b.maxworkers; i++ {
+		newWorker(b.CommandHandler, b.workerPool)
 	}
 }
